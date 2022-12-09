6b0b08029956798c17bec4dd1c33f748234d5499
John Adedigba <john.adedigba@digitalturbine.com>
2022-12-09 10:16:27 +0100

    cache kubetools dependencies

--- .circleci/kube-tools.sh
+++ .circleci/kube-tools.sh
@@ -15,21 +15,21 @@ fi
 # Check if yq in tools folder
 if [ -f "/tmp/tools/yq" ]; then
     echo "yq already installed"
-    chmod +x /tmp/tools/yq && sudo mv /tmp/tools/yq /usr/bin/yq
+    chmod +x /tmp/tools/yq && sudo cp /tmp/tools/yq /usr/bin/yq
 else
     echo "Installing yq"
     wget https://github.com/mikefarah/yq/releases/download/v4.16.2/yq_linux_arm64 -O /tmp/tools/yq
-    chmod +x /tmp/tools/yq && sudo mv /tmp/tools/yq /usr/bin/yq
+    chmod +x /tmp/tools/yq && sudo cp /tmp/tools/yq /usr/bin/yq
 fi
 
 # Check if helm in tools folder
 if [ -f "/tmp/tools/helm" ]; then
     echo "helm already installed"
-    chmod +x /tmp/tools/helm && sudo mv /tmp/tools/helm /usr/local/bin/helm
+    chmod +x /tmp/tools/helm && sudo cp /tmp/tools/helm /usr/local/bin/helm
 else
     echo "Installing helm"
     curl -LO https://get.helm.sh/helm-v3.7.2-linux-arm64.tar.gz
     tar -zxvf /tmp/tools/helm-v3.7.2-linux-arm64.tar.gz
-    sudo mv /tmp/tools/linux-arm64/helm /tmp/tools/helm
-    sudo mv /tmp/tools/helm /usr/local/bin/helm
+    sudo cp /tmp/tools/linux-arm64/helm /tmp/tools/helm
+    sudo cp /tmp/tools/helm /usr/local/bin/helm
 fi
